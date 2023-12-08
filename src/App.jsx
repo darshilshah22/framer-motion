be8d0a81df77c441518cbda6bffbd2595d8f443d
darshilshah22 <shah.darshilp@gmail.com>
2023-12-09 01:31:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { motion } from "framer-motion"
+
+function App() {
+  return (
+    <div className="App">
+      <motion.div className="square"></motion.div>
+    </div>
+  );
+}
+
+export default App;
